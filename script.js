2cad1a1ce75641b461323352b1af43aa75fff4ad
Aura Marquez <amarquezmalpica@gmail.com>
2025-06-22 14:40:57 -0600

    feat: video player new functionality

--- script.js
+++ script.js
@@ -1,40 +1,115 @@
-let video = document.querySelector(".video-display");
+let videoContainer = document.querySelector(".video-container");
 let playButton = document.querySelector(".play-btn");
 let muteButton = document.querySelector(".mute-btn");
 let speedLabel = document.querySelector(".speed-lbl");
 let progressBar = document.querySelector(".progress-bar");
+let video = null;
+
+const videos = [
+  {
+    id: "i-dont-love-you",
+    file: "IDontLoveYou_MyChemicalRomance.mp4",
+    subtitle: "",
+  },
+  {
+    id: "taste",
+    file: "Taste_SabrinaCarpenter.mp4",
+    subtitle: "Taste_SabrinaCarpenter.vtt",
+  },
+  {
+    id: "tear-you-apart",
+    file: "TearYouApart_SheWantsRevenge.mp4",
+    subtitle: "TearYouApart_SheWantsRevenge.vtt",
+  },
+  {
+    id: "the-less-i-know",
+    file: "TheLessIKnowTheBetter_TameImpala.mp4",
+    subtitle: "TheLessIKnowTheBetter_TameImpala.vtt",
+  },
+  {
+    id: "you-know-im",
+    file: "YouKnowImNoGood_AmyWinehouse.mp4",
+    subtitle: "YouKnowImNoGood_AmyWinehouse.vtt",
+  },
+];
 
 const videoSizes = [
   { width: 256, height: 144 },
   { width: 512, height: 288 },
   { width: 768, height: 432 },
   { width: 1024, height: 576 },
-  { width: 1280, height: 720 },
+  // { width: 1280, height: 720 },
 ];
 let sizeIndex = 2;
+let videoId = "the-less-i-know";
+let showSubtitles = false;
 
-video.addEventListener('loadedmetadata', function () {
-  progressBar.value = 0;
-  progressBar.setAttribute('max', video.duration);
-});
+// window.addEventListener("load", (event) => {
+//   let trackElem = document.querySelector("track");
+//   let track = trackElem.track;
 
-video.addEventListener("ended", function () {
-  video.currentTime = 0;
-  playButton.textContent = 'PLAY';
-});
+//   track.mode = "showing";
 
-video.addEventListener('timeupdate', function () {
-  progressBar.value = video.currentTime;
-});
+//   for (const cue of track.cues) {
+//     cue.pauseOnExit = true;
+//   }
+// });
+
+function createVideoPlayer() {
+  video = document.createElement("video");
+  video.className = "video-display";
+  video.width = videoSizes[sizeIndex].width;
+  video.height = videoSizes[sizeIndex].height;
+
+  video.addEventListener("loadedmetadata", function () {
+    progressBar.value = 0;
+    progressBar.setAttribute("max", video.duration);
+  });
+
+  video.addEventListener("ended", function () {
+    video.currentTime = 0;
+    playButton.textContent = "PLAY";
+  });
+
+  video.addEventListener("timeupdate", function () {
+    progressBar.value = video.currentTime;
+  });
+
+  const newVideo = videos.find((item) => item.id === videoId);
+
+  const source = document.createElement("source");
+  source.src = `./videos/${newVideo.file}`;
+  source.type = "video/mp4";
+  video.appendChild(source);
+
+  if (newVideo.subtitle) {
+    const track = document.createElement("track");
+    track.src = `./videos/${newVideo.subtitle}`;
+    track.srclang = "en";
+    track.classList = "subtitles";
+    video.appendChild(track);
+  }
+
+  videoContainer.append(video);
+}
+
+function playVideo(newVideoId) {
+  video.pause();
+
+  videoId = newVideoId;
+  videoContainer.innerHTML = "";
+  createVideoPlayer();
+
+  video.play();
+}
 
 function play() {
   if (video.paused) {
     video.play();
-    playButton.textContent = 'PAUSE';
-  }
-  else {
+    playButton.textContent = "PAUSE";
+  } else {
     video.pause();
-    playButton.textContent = 'PLAY';
+    playButton.textContent = "PLAY";
   }
 }
 
@@ -42,32 +117,46 @@ function shiftVideo() {
   video.currentTime = progressBar.value;
 }
 
+function changeVideo(operation) {
+  let videoIndex = videos.findIndex(item => item.id === videoId);
+
+  if (operation === "previous" && videoIndex > 0) {
+    videoIndex -= 1;
+  } else if (operation === "next" && videoIndex < videos.length -1) {
+    videoIndex += 1;
+  } else {
+    return;
+  }
+
+  playVideo(videos[videoIndex].id)
+}
+
 function mute() {
   video.muted = !video.muted;
 
   if (video.muted) {
-    muteButton.textContent = 'UNMUTE';
+    muteButton.textContent = "UNMUTE";
   } else {
-    muteButton.textContent = 'MUTE';
+    muteButton.textContent = "MUTE";
   }
 }
 
 function changeVolume(operation) {
   const currentVolume = Math.floor(video.volume * 10) / 10;
 
-  if (operation === 'decrease' && currentVolume > 0) {
+  if (operation === "decrease" && currentVolume > 0) {
     video.volume -= 0.1;
-  } else if (operation === 'increase' && currentVolume < 1) {
+  } else if (operation === "increase" && currentVolume < 1) {
     video.volume += 0.1;
   }
 }
 
 function changeSpeed(operation) {
-  if (operation === 'slower' && video.playbackRate > 0.25) {
+  if (operation === "slower" && video.playbackRate > 0.25) {
     video.playbackRate -= 0.25;
-  } else if (operation === 'faster') {
+  } else if (operation === "faster") {
     video.playbackRate += 0.25;
-  } else if (operation === 'default') {
+  } else if (operation === "default") {
     video.playbackRate = 1;
   }
 
@@ -75,9 +164,9 @@ function changeSpeed(operation) {
 }
 
 function changeSize(operation) {
-  if (operation === 'down' && sizeIndex > 0) {
+  if (operation === "down" && sizeIndex > 0) {
     sizeIndex -= 1;
-  } else if (operation === 'up' && sizeIndex < videoSizes.length - 1) {
+  } else if (operation === "up" && sizeIndex < videoSizes.length - 1) {
     sizeIndex += 1;
   }
 
@@ -85,3 +174,15 @@ function changeSize(operation) {
   video.height = videoSizes[sizeIndex].height;
 }
 
+function toogleSubtitles() {
+  let subtitules = document.querySelector(".subtitles");
+  let track = subtitules.track;
+
+  showSubtitles = !showSubtitles;
+
+  if (showSubtitles) {
+    track.mode = "showing";
+  } else {
+    track.mode = "hidden";
+  }
+}
